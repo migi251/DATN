0af462996a67b3c0d6fa38ff93165ac9f3b4e118
Migi251 <phambatoan251@gmail.com>
2019-05-10 02:19:53 +0700

    rotate 180 to 30

--- torchtext/transforms.py
+++ torchtext/transforms.py
@@ -94,7 +94,7 @@ class RandomBrightness(object):
 
 
 class Rotate(object):
-    def __init__(self, up=180):
+    def __init__(self, up=30):
         self.up = up
 
     def rotate(self, center, pt, theta):  # 二维图形学的旋转
