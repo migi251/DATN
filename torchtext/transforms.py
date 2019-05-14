987aa762bed9642cefb792ea76f622e26066fcce
Migi251 <phambatoan251@gmail.com>
2019-05-14 22:10:53 +0700

    add mirror transform

--- torchtext/transforms.py
+++ torchtext/transforms.py
@@ -366,9 +366,9 @@ class Augmentation(object):
             # Padding(),
             # RandomBrightness(),
             # RandomContrast(),
-            # RandomMirror(),
+            RandomMirror(),
             RandomResizedLimitCrop(
-                maxHeight=maxHeight, maxWidth=maxWidth, scale=(0.1, 1.0), ratio=(0.3, 3)),
+                maxHeight=maxHeight, maxWidth=maxWidth, scale=(0.2, 1.0), ratio=(0.3, 3)),
             Rotate(),
             # Resize(maxHeight=maxHeight, maxWidth=maxWidth),
             Normalize(mean, std)
