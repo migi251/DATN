d2133a91ce5dafb2897fbd005ed509596906c625
Migi251 <phambatoan251@gmail.com>
2019-06-10 02:53:02 +0700

    add IC17 dataset

--- torchtext/datasets/__init__.py
+++ torchtext/datasets/__init__.py
@@ -4,6 +4,7 @@ from .synth_text_2 import SynthText_2
 from .ctw1500 import CTW1500
 from .ic13 import IC13
 from .ic15 import IC15
+from .ic17 import IC17
 
 __detect_factory = {
     'total-text': TotalText,
@@ -11,7 +12,8 @@ __detect_factory = {
     'synth-text-2': SynthText_2,
     'ctw1500': CTW1500,
     'IC13': IC13,
-    'IC15':IC15
+    'IC15':IC15,
+    'IC17':IC17
 }
 
 
